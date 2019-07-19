4e6dfaaadaf7e861d019e822b51226d45b9c7d57
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 23:42:58 -0400

    Injecting ApiCallInterface into WebDataStore class. Added comments.

--- app/src/main/java/com/gideon/weather/repos/WebDataStore.java
+++ app/src/main/java/com/gideon/weather/repos/WebDataStore.java
@@ -1,12 +1,27 @@
 package com.gideon.weather.repos;
 
+import com.gideon.weather.api.ApiCallInterface;
 import com.gideon.weather.models.WeatherData;
+
+import javax.inject.Inject;
 
 import io.reactivex.Observable;
 
+/**
+ * This class is responsible to retrieve the weather data from online source.
+ * 
+ */
 public class WebDataStore implements RepoInterface{
 
     public static String TAG = "WebDataStore";
+
+    private ApiCallInterface apiCallInterface;
+
+    @Inject
+    public WebDataStore(ApiCallInterface apiCallInterface){
+        this.apiCallInterface = apiCallInterface;
+    }
+
     @Override
     public Observable<WeatherData> downloadWeatherData(String lat, String lon) {
         return null;
