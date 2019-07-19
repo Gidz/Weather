13593247e8852d24f469f06b7dc91b342579a798
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 21:43:22 -0400

    Initial commit

---
+++ app/src/main/java/com/gideon/weather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gideon.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
