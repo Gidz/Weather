638025fd60470487bb112cedf49902b82b0ea78d
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 22:15:14 -0400

    Added a Constants class which can hold static variables to be used by the rest of the project.

---
+++ app/src/main/java/com/gideon/weather/base/Constants.java
@@ -0,0 +1,7 @@
+package com.gideon.weather.base;
+
+public class Constants {
+    public static String  API_KEY = "35001d36c28931e7e740eb4337b1c813";
+    public static String  BASE_URL = "https://api.darksky.net/forecast/"+ API_KEY+"/";
+    public static String SHARED_PREFERENCES_KEY = "WEATHER_PREFERENCES";
+}
