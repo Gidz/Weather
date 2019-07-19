18073a437c7260bbca7b3c4f09a99aafa56f4139
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 22:29:44 -0400

    Added the API calls.

--- app/src/main/java/com/gideon/weather/api/ApiCallInterface.java
+++ app/src/main/java/com/gideon/weather/api/ApiCallInterface.java
@@ -1,4 +1,16 @@
 package com.gideon.weather.api;
 
-public class ApiCallInterface {
+import com.gideon.weather.models.WeatherData;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiCallInterface {
+
+    /*Add all the APi calls here*/
+
+    @GET("{coordinates}")
+    Call<WeatherData> getWeatherData(@Path("coordinates") String latitudeAndLongitude);
+
 }
