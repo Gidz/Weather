ec8b1b38d169c1612d08f79cf1828ac19f269b29
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 21:55:10 -0400

    Filled in code for custom App class; Added support for Java 8 in gradle

--- app/src/main/java/com/gideon/weather/base/App.java
+++ app/src/main/java/com/gideon/weather/base/App.java
@@ -1,11 +1,13 @@
 package com.gideon.weather.base;
 
+import com.gideon.weather.di.DaggerAppComponent;
+
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
 public class App extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
+        return DaggerAppComponent.builder().application(this).build();
     }
 }
