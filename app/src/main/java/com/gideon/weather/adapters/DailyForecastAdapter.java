8756dbe7a25b0dbce0a1be75076d8e2ab6838039
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 23:52:05 -0400

    Added skeleton code for DailyForecast list.

---
+++ app/src/main/java/com/gideon/weather/adapters/DailyForecastAdapter.java
@@ -0,0 +1,9 @@
+package com.gideon.weather.adapters;
+
+/**
+ * We will need to display a Recyclerview with daily forecast.
+ * That's when this adapter comes in handy.
+ */
+//TODO: Extend RecyclerView.Adapter
+public class DailyForecastAdapter {
+}
