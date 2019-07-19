815973b3a4aad2c2cce5a7ed7bd415db7e8441e8
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 23:18:32 -0400

    Added 2 paths for program flow - one if location wasn't set, one if it was.

--- app/src/main/java/com/gideon/weather/WeatherActivity.java
+++ app/src/main/java/com/gideon/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.gideon.weather;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.lifecycle.ViewModelProviders;
 
@@ -15,10 +17,16 @@ public class WeatherActivity extends DaggerAppCompatActivity {
 
     private WeatherActivityViewModel weatherActivityViewModel;
 
+    String lat;
+    String lon;
+
 
     //Dependency injection
     @Inject
     ViewModelProviderFactory viewModelProviderFactory;
+
+    @Inject
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +41,27 @@ public class WeatherActivity extends DaggerAppCompatActivity {
     }
 
     private void checkLocationPreferences() {
+        // NYC Coordinates : 40.7128° N, 74.0060° W
+
+        //If the requested value wasn't present i.e., if lat or lon value wasn't in the
+        // shared preferences, "UNKNOWN" will be returned as a value.
+        lat = sharedPreferences.getString("lat", "UNKNOWN");
+        lon = sharedPreferences.getString("lon", "UNKNOWN");
+
+        //If the location wasn't set, set the location first.
+        //Else, observer for any data changes. (Rx)
+        if (lat.equals("UNKNOWN") || lon.equals("UNKNOWN")) {
+            //The location wasn't set before. Set it.
+            setLocation();
+        } else {
+            //The location was set before
+            observeDataChanges();
+        }
+    }
+
+    private void observeDataChanges() {
+    }
+
+    private void setLocation() {
     }
 }
