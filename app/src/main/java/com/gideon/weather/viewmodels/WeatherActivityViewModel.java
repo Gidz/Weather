7143ac57d4bc1feb99e18310007aa3a3273719fc
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 23:58:48 -0400

    Getting the observable in the view model.

--- app/src/main/java/com/gideon/weather/viewmodels/WeatherActivityViewModel.java
+++ app/src/main/java/com/gideon/weather/viewmodels/WeatherActivityViewModel.java
@@ -2,23 +2,26 @@ package com.gideon.weather.viewmodels;
 
 import androidx.lifecycle.ViewModel;
 
+import com.gideon.weather.models.WeatherData;
 import com.gideon.weather.repos.WeatherRepo;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observable;
 
 public class WeatherActivityViewModel extends ViewModel {
 
     private static String TAG = "WeatherActivityViewModel";
 
     private WeatherRepo weatherRepo;
+    private Observable<WeatherData> weatherDataObservable;
 
     @Inject
     public WeatherActivityViewModel(WeatherRepo weatherRepo) {
         this.weatherRepo = weatherRepo;
     }
-
-    //TODO: Request from the repo and download data
+    
     public void downloadWeatherData(String lat, String lon) {
-
+        weatherDataObservable = weatherRepo.downloadWeatherData(lat, lon);
     }
 }
