8be7d7bc73182b39e4823b84dee77a16d573f532
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 23:08:17 -0400

    All network related modules are now ready.

--- app/src/main/java/com/gideon/weather/di/AppModule.java
+++ app/src/main/java/com/gideon/weather/di/AppModule.java
@@ -1,7 +1,49 @@
 package com.gideon.weather.di;
 
+import com.gideon.weather.api.ApiCallInterface;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.gideon.weather.base.Constants.BASE_URL;
 
 @Module
 public class AppModule {
+
+    /*Network related modules*/
+    @Provides
+    @Singleton
+    public RxJava2CallAdapterFactory provideRxJava2CallAdapterFactory() {
+        return RxJava2CallAdapterFactory.create();
+    }
+
+    @Singleton
+    @Provides
+    public GsonConverterFactory provideGsonConverterFactory() {
+        return GsonConverterFactory.create();
+    }
+
+    @Provides
+    @Singleton
+    public Retrofit provideRetrofit(GsonConverterFactory gsonConverterFactory, RxJava2CallAdapterFactory rxJava2CallAdapterFactory) {
+        Retrofit client;
+        client = new retrofit2.Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(rxJava2CallAdapterFactory)
+                .addConverterFactory(gsonConverterFactory)
+                .build();
+
+        return client;
+    }
+
+    @Singleton
+    @Provides
+    ApiCallInterface getApi(Retrofit retrofit) {
+        return retrofit.create(ApiCallInterface.class);
+    }
 }
