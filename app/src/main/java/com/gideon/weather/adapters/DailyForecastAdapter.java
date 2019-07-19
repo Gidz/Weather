ef7d147a5a8208fbf84edfb0e5938e53f0dc47d3
Gideon Paul <gidzpaul95@gmail.com>
2019-07-19 00:28:05 -0400

    Created a list and a setter for RecyclerView class.

--- app/src/main/java/com/gideon/weather/adapters/DailyForecastAdapter.java
+++ app/src/main/java/com/gideon/weather/adapters/DailyForecastAdapter.java
@@ -6,12 +6,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.gideon.weather.models.DailyData;
+
+import java.util.List;
+
 /**
  * We will need to display a Recyclerview with daily forecast.
  * That's when this adapter comes in handy.
  */
 //TODO: Extend RecyclerView.Adapter
 public class DailyForecastAdapter extends RecyclerView.Adapter<DailyForecastAdapter.DailyForecastAdapterViewHolder> {
+
+    private List<DailyData> dailyDataList;
 
     @NonNull
     @Override
@@ -33,5 +39,9 @@ public class DailyForecastAdapter extends RecyclerView.Adapter<DailyForecastAdap
         public DailyForecastAdapterViewHolder(@NonNull View itemView) {
             super(itemView);
         }
+    }
+
+    public void setDailyDataList(List<DailyData> dailyDataList) {
+        this.dailyDataList = dailyDataList;
     }
 }
