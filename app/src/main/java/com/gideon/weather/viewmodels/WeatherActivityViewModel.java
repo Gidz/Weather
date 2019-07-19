0fdff7634e95c05789867b65dfe34538f4aebfef
Gideon Paul <gidzpaul95@gmail.com>
2019-07-18 23:53:23 -0400

    WeatherViewModel now has WeatherRepo object provided by Dagger.

--- app/src/main/java/com/gideon/weather/viewmodels/WeatherActivityViewModel.java
+++ app/src/main/java/com/gideon/weather/viewmodels/WeatherActivityViewModel.java
@@ -2,13 +2,18 @@ package com.gideon.weather.viewmodels;
 
 import androidx.lifecycle.ViewModel;
 
+import com.gideon.weather.repos.WeatherRepo;
+
 import javax.inject.Inject;
 
 public class WeatherActivityViewModel extends ViewModel {
 
     private static String TAG = "WeatherActivityViewModel";
 
+    private WeatherRepo weatherRepo;
+
     @Inject
-    public WeatherActivityViewModel() {
+    public WeatherActivityViewModel(WeatherRepo weatherRepo) {
+        this.weatherRepo = weatherRepo;
     }
 }
